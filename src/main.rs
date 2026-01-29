54ce3f04dfd58cb985d0810d7eb48b53b51afb8d
zhangyemengren <624593758@qq.com>
2026-01-29 17:32:09 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -2,10 +2,11 @@ mod config;
 mod fetch;
 
 use config::Config;
-
+use fetch::fetch_chat;
 
 #[tokio::main]
 async fn main() {
     let cfg = Config::from_env().unwrap();
     println!("Loaded config: api_key length = {}", cfg.api_key.len());
+    fetch_chat(&cfg).await.unwrap();
 }
